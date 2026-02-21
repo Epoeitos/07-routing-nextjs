e44e0442f2a87de53d04c089ed4d9bbda5d0e1fa
Sasha <sasha21100@ukr.net>
2026-02-21 23:13:59 +0200

    fix

--- .next/server/app/index.segments/@modal/__DEFAULT__.segment.rsc
+++ .next/server/app/index.segments/@modal/__DEFAULT__.segment.rsc
@@ -1,2 +1,2 @@
 1:"$Sreact.fragment"
-0:{"buildId":"VIozqLWFp7ps0tGzNdMwf","rsc":["$","$1","c",{"children":[null,null,null]}],"loading":null,"isPartial":false}
+0:{"buildId":"dpV60gtoNAwlUbUvv7GmD","rsc":["$","$1","c",{"children":[null,null,null]}],"loading":null,"isPartial":false}
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,11 +8,11 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: Wiktor Fox</p>
+          <p>Developer: Furman Oleksandr</p>
           <p>
             Contact us:
             <Link
-              href="https://www.linkedin.com/in/wiktor-bruj/"
+              href="https://www.linkedin.com/in/oleksandr-furman-0a99a1372/"
               target="_blank"
             >
               Linkedin
